ff63c00e11792092651ad366c1eec663727f10cb
haki36 <yildirim.h@hotmail.com>
2026-03-08 21:55:45 +0100

    feat: add flight search by date to data access layer

--- flights_data.py
+++ flights_data.py
@@ -1,6 +1,22 @@
 from sqlalchemy import create_engine, text
 
-QUERY_FLIGHT_BY_ID = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.ID = :id"
+QUERY_FLIGHT_BY_ID = (
+    "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, "
+    "flights.DEPARTURE_DELAY as DELAY "
+    "FROM flights "
+    "JOIN airlines ON flights.airline = airlines.id "
+    "WHERE flights.ID = :id"
+)
+
+QUERY_FLIGHT_BY_DATE = (
+    "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, "
+    "flights.DEPARTURE_DELAY as DELAY "
+    "FROM flights "
+    "JOIN airlines ON flights.airline = airlines.id "
+    "WHERE flights.DAY = :day "
+    "AND flights.MONTH = :month "
+    "AND flights.YEAR = :year"
+)
 
 # Define the database URL
 DATABASE_URL = "sqlite:///data/flights.sqlite3"
@@ -34,3 +50,14 @@ def get_flight_by_id(flight_id):
     """
     params = {'id': flight_id}
     return execute_query(QUERY_FLIGHT_BY_ID, params)
+
+
+def get_flights_by_date(flight_day, flight_month,flight_year):
+    """
+    Searches for flight details using flight Date.
+    If the flight was found, returns a list with records.
+    """
+    params = {'day': flight_day,
+              'month': flight_month,
+              'year': flight_year}
+    return execute_query(QUERY_FLIGHT_BY_DATE, params)
